794f2abd2ec6f84eddc645e61628abc396113b89
Arnold Okoth <arnold.okoth@andela.com>
2016-10-02 20:03:37 +0300

    Changed project structure: added models folder that will hold all classes

---
+++ models/tests/test_amity.py
@@ -0,0 +1,50 @@
+import unittest
+
+from amity.amity import Amity
+
+class TestAmity(unittest.TestCase):
+
+    def setUp(self):
+        self.amity_object = Amity()
+
+    def test_is_instance_of(self):
+        self.assertIsInstance(self.amity_object, Amity)
+
+    def test_is_object_of(self):
+        self.assertTrue(type(self.amity_object) is Amity)
+
+    @unittest.skip("work in progress")
+    def test_create_room(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_add_person(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_reallocate_person(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_load_people(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_print_allocations(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_print_room(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_save_state(self):
+        pass
+
+    @unittest.skip("work in progress")
+    def test_load_state(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
