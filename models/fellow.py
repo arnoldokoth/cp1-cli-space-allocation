012a70879fafdb9b2ffd899267ab2cccad32bed1
andela-aokoth <arnold.okoth@andela.com>
2016-10-21 17:25:47 +0300

    Remove cache lists in class staff & fellow

--- models/fellow.py
+++ models/fellow.py
@@ -2,14 +2,10 @@ from models.person import Person
 
 
 class Fellow(Person):
-	fellow_cache = []
 
-	def __init__(self, first_name, last_name):
-		Person.__init__(self, first_name, last_name)
-		full_name = self.first_name + " " + self.last_name
-		Fellow.fellow_cache.append(full_name)
+    def __init__(self, first_name, last_name):
+        Person.__init__(self, first_name, last_name)
 
-	@staticmethod
-	def get_fellow_cache():
-		return Fellow.fellow_cache
+
+
 
